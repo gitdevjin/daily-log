0551b827362fa4c7d7c62b78253377ad989cdfa1
gitdevjin <dev.hjshin@gmail.com>
2025-11-27 18:28:33 -0500

    project initiation

---
+++ src/components/layout/global-layout.tsx
@@ -0,0 +1,31 @@
+import { Link, Outlet } from "react-router";
+import logo from "@/assets/logo.png";
+import defaultAvatar from "@/assets/default-avatar.jpg";
+import { SunIcon } from "lucide-react";
+
+export default function GlobalLayout() {
+  return (
+    <div className="flex min-h-[100vh] flex-col">
+      <header className="h-15 border-b">
+        <div className="m-auto flex h-full w-full max-w-175 justify-between px-4">
+          <Link to={"/"} className="flex items-center gap-2">
+            <img className="h-5" src={logo} alt="Logo of Daily Log" />
+            <div className="font-bold">Daily Log</div>
+          </Link>
+          <div className="flex items-center gap-5">
+            <div className="hover:bg-muted cursor-pointer rounded-full p-2">
+              <SunIcon />
+            </div>
+            <img className="h-6" src={defaultAvatar} alt="" />
+          </div>
+        </div>
+      </header>
+      <main className="m-auto w-full max-w-175 flex-1 border-x px-4 py-6">
+        <Outlet />
+      </main>
+      <footer className="text-muted-foreground border-t py-10 text-center">
+        @Jin
+      </footer>
+    </div>
+  );
+}
