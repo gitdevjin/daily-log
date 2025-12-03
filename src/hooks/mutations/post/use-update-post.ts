6b61feb2ebbdcd78bb663ff6fc83b72acd3cc408
gitdevjin <dev.hjshin@gmail.com>
2025-12-03 01:50:40 -0500

    authorization added

--- src/hooks/mutations/post/use-update-post.ts
+++ src/hooks/mutations/post/use-update-post.ts
@@ -1,12 +1,25 @@
 import { updatePost } from "@/api/post";
-import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import type { Post, UseMutationCallback } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useUpdatePost(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: updatePost,
-    onSuccess: () => {
+    onSuccess: (updatedPost) => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
+
+      queryClient.setQueryData<Post>(
+        QUERY_KEYS.post.byId(updatedPost.id),
+        (prevPost) => {
+          if (!prevPost)
+            throw new Error(
+              `${updatedPost.id} Post Doesn't exist in Cache Data`,
+            );
+          return { ...prevPost, ...updatedPost };
+        },
+      );
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
